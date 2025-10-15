1ef98a213cf517097efb2da54822dc4b2e6b09a9
cdarino <carino@addu.edu.ph>
2025-10-15 13:56:50 +0800

    Uploaded project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import org.example.gui.Mainframe;
+import org.example.gui.utils.fonts.fontLoader;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        fontLoader.loadFonts();
+
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new Mainframe().setVisible(true);
+        });
+    }
+}
